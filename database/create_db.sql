3466996417d0f3407879d5b26d1521a0e8fa2a04
Murtaz <itsmurtaz@gmail.com>
2023-11-11 01:26:09 +0530

    Adding requests package to the requirements.txt Added check in sql file not create database if it already exists

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,2 +1,4 @@
-CREATE DATABASE courseregistrationservice;
+SELECT 'CREATE DATABASE courseregistrationservice'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'courseregistrationservice')\gexec;
+
 GRANT ALL PRIVILEGES ON DATABASE courseregistrationservice TO postgres;
