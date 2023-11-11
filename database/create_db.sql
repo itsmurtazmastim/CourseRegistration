1556184d8cc11339f7b4bce39d3f9b9b522d8e30
VaniDevi <vanidevops5@gmail.com>
2023-11-12 00:06:33 +0530

    changed sql file

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,4 +1,11 @@
-SELECT 'CREATE DATABASE courseregistrationservice'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'courseregistrationservice')\gexec;
+--SELECT 'CREATE DATABASE courseregistrationservice'
+--WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'courseregistrationservice')\gexec;
 
-GRANT ALL PRIVILEGES ON DATABASE courseregistrationservice TO postgres;
+--GRANT ALL PRIVILEGES ON DATABASE courseregistrationservice TO postgres;
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'courseregistrationservice') THEN
+    CREATE DATABASE courseregistrationservice;
+    GRANT ALL PRIVILEGES ON DATABASE courseregistrationservice TO postgres;
+  END IF;
+END $$;
